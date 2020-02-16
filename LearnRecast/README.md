6ac3a1ae7fd212648fe8c49772efa6d2635b6688
npangunion <npangunion@gmail.com>
2020-02-16 13:51:09 +0900

    - 프로젝트 생성

---
+++ LearnRecast/README.md
@@ -0,0 +1,5 @@
+# wise.scratchpad.LearnRecast 
+
+Recast 데모를 수정하여 다양한 시도를 해본다. 주로 Crowd의 성능 위주로 테스트를 진행한다. 
+프로젝트는 소스를 직접 포함하도록 한다. 
+
---
+++ LearnRecast/premake5.lua
@@ -0,0 +1,53 @@
+local BOOST_HOME = os.getenv("BOOST_HOME")
+
+workspace "LearnRecast"
+	-- We set the location of the files Premake will generate
+	location "generated"
+	
+	-- We indicate that all the projects are C++ only
+	language "C++"
+	
+	-- We will compile for x86_64. You can change this to x86 for 32 bit builds.
+	architecture "x86_64"
+	
+	-- Configurations are often used to store some compiler / linker settings together.
+    -- The Debug configuration will be used by us while debugging.
+    -- The optimized Release configuration will be used when shipping the app.
+	configurations { "Debug", "Release" }
+	
+	-- We use filters to set options, a new feature of Premake5.
+	
+	-- We now only set settings for the Debug configuration
+	filter { "configurations:Debug" }
+		-- We want debug symbols in our debug config
+		symbols "On"
+	
+	-- We now only set settings for Release
+	filter { "configurations:Release" }
+		-- Release should be optimized
+		optimize "On"
+	
+	-- Reset the filter for other settings
+	filter { }
+	
+	-- Here we use some "tokens" (the things between %{ ... }). They will be replaced by Premake
+	-- automatically when configuring the projects.
+	-- * %{prj.name} will be replaced by "ExampleLib" / "App" / "UnitTests"
+	--  * %{cfg.longname} will be replaced by "Debug" or "Release" depending on the configuration
+	-- The path is relative to *this* folder
+	targetdir ("build/bin/%{prj.name}/%{cfg.longname}")
+    objdir ("build/obj/%{prj.name}/%{cfg.longname}")
+	
+function includeBOOST()
+	includedirs (BOOST_HOME .. "/include")
+end	
+
+project "GoRecast"
+	kind "ConsoleApp"
+	
+	files { "include/**", "src/**" }
+	
+	includeBOOST()
+
+	
+    
