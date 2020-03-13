607dcb87d3ee0cc593327a4fda346a4bbe09f8f2
npangunion <npangunion@gmail.com>
2020-03-13 17:49:27 +0900

    - EmbedIO로 동작 확인

--- LearnHttpServer/Program.cs
+++ LearnHttpServer/Program.cs
@@ -2,104 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using EmbedIO;
 
 namespace LearnHttpServer
 {
     class Program
     {
-        class HttpServer
-        {
-            public static HttpListener listener;
-            public static string url = "http://localhost:8000/";
-            public static int pageViews = 0;
-            public static int requestCount = 0;
-            public static string pageData =
-                "<!DOCTYPE>" +
-                "<html>" +
-                "  <head>" +
-                "    <title>HttpListener Example</title>" +
-                "  </head>" +
-                "  <body>" +
-                "    <p>Page Views: {0}</p>" +
-                "    <form method=\"post\" action=\"shutdown\">" +
-                "      <input type=\"submit\" value=\"Shutdown\" {1}>" +
-                "    </form>" +
-                "  </body>" +
-                "</html>";
-
-            public async void Start()
-            {
-                // Create a Http server and start listening for incoming connections
-                listener = new HttpListener();
-                listener.Prefixes.Add(url);
-                listener.Start();
-                Console.WriteLine("Listening for connections on {0}", url);
-
-                // Handle requests
-                await HandleIncomingConnections();
-                // listenTask.GetAwaiter().GetResult();
-
-                // Close the listener
-                listener.Close();
-            }
-
-            public async Task HandleIncomingConnections()
-            {
-                bool runServer = true;
-
-                // While a user hasn't visited the `shutdown` url, keep on handling requests
-                while (runServer)
-                {
-                    // Will wait here until we hear from a connection
-                    HttpListenerContext ctx = await listener.GetContextAsync();
-
-                    // Peel out the requests and response objects
-                    HttpListenerRequest req = ctx.Request;
-                    HttpListenerResponse resp = ctx.Response;
-
-                    // Print out some info about the request
-                    Console.WriteLine("Request #: {0}", ++requestCount);
-                    Console.WriteLine(req.Url.ToString());
-                    Console.WriteLine(req.HttpMethod);
-                    Console.WriteLine(req.UserHostName);
-                    Console.WriteLine(req.UserAgent);
-                    Console.WriteLine();
-
-                    // If `shutdown` url requested w/ POST, then shutdown the server after serving the page
-                    if ((req.HttpMethod == "POST") && (req.Url.AbsolutePath == "/shutdown"))
-                    {
-                        Console.WriteLine("Shutdown requested");
-                        runServer = false;
-                    }
-
-                    // Make sure we don't increment the page views counter if `favicon.ico` is requested
-                    if (req.Url.AbsolutePath != "/favicon.ico")
-                        pageViews += 1;
-
-                    // Write the response info
-                    string disableSubmit = !runServer ? "disabled" : "";
-                    byte[] data = Encoding.UTF8.GetBytes(String.Format(pageData, pageViews, disableSubmit));
-                    resp.ContentType = "text/html";
-                    resp.ContentEncoding = Encoding.UTF8;
-                    resp.ContentLength64 = data.LongLength;
-
-                    // Write out to the response stream (asynchronously), then close it
-                    await resp.OutputStream.WriteAsync(data, 0, data.Length);
-                    resp.Close();
-                }
-            }
-        }
-
-
         public static void Main(string[] args)
         {
-            var server = new HttpServer();
+            // var url = "http://localhost:9696/";
+            var url = "http://10.95.1.236:9696/";
+            if (args.Length > 0)
+                url = args[0];
 
-            server.Start();
+            // Our web server is disposable.
+            using (var server = CreateWebServer(url))
+            {
+                // Once we've registered our modules and configured them, we call the RunAsync() method.
+                server.RunAsync();
 
-            System.Threading.Thread.Sleep(100000);
+                System.Threading.Thread.Sleep(100000);
+            }
         }
-    }
+
+        // Create and configure our web server.
+        private static WebServer CreateWebServer(string url)
+        {
+            var server = new WebServer(o => o
+                    .WithUrlPrefix(url)
+                    .WithMode(HttpListenerMode.EmbedIO))
+                // First, we will configure our web server by adding Modules.
+                .WithLocalSessionManager();
+
+            // Listen for state changes.
+            // server.StateChanged += (s, e) => $"WebServer New State - {e.NewState}".Info();
+
+            return server;
+        }
+    } 
 }
